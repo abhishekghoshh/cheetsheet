0ecbffe2cbf7bc4c08723ebe498946c188dac3ea
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-09-20 18:08:28 +0530

    heap completed

--- DSA/heap/max-heap.c
+++ DSA/heap/max-heap.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-#include<conio.h>
+// #include<conio.h>
 #include<stdlib.h>
 struct heap{
     int heapSize;
-    int length;
     int *items;
+    int length;
 };
 struct heap create(int length){
     struct heap heap_;
-    heap_.length=length;
     heap_.items=(int*)malloc(length*sizeof(int*));
-    heap_.heapSize=0;
+    heap_.heapSize=length;
+    heap_.length=length;
+    for(int i=0;i<length;i++){
+        heap_.items[i]=rand()%length;
+    }
     return heap_;
 }
 void swap(int *first ,int *second){
@@ -18,9 +21,19 @@ void swap(int *first ,int *second){
     *first=*second;
     *second=temp;
 }
+int getParent(int key){
+    return (key/2)-1;
+}
+int getRightChild(int key){
+    return 2*key+2;
+}
+int getLeftChild(int key){
+    return 2*key+1;
+}
+
 void maxHeapify(struct heap *heap_,int start){
-    int left = 2*start+1;
-    int right = 2*start+2;
+    int left = getLeftChild(start);
+    int right = getRightChild(start);
     int largest;
     if(left < heap_->heapSize && heap_->items[left]>heap_->items[start]){
         largest=left;
@@ -35,28 +48,81 @@ void maxHeapify(struct heap *heap_,int start){
         maxHeapify(heap_,largest);
     }
 }
-void buildHeap(struct heap *heap){
-    heap->heapSize=heap->length;
-    int i=((heap->length)/2)-1;
+void buildHeap(struct heap *heap_){
+    int i=((heap_->heapSize)/2)-1;
     while(i>=0){
-        maxHeapify(heap,i);
+        maxHeapify(heap_,i);
         i--;
     }
 }
+int extractMax(struct heap *heap_){
+    int max = -99999;
+    if(heap_->heapSize>0){
+        max=heap_->items[0];
+        heap_->items[0]=heap_->items[heap_->heapSize-1];
+        heap_->heapSize=heap_->heapSize-1;
+        maxHeapify(heap_,0);
+    }
+    return max;
+}
 void print(struct heap heap_){
+    printf("Items in the heap are : ");
+    for(int i=0;i<heap_.heapSize;i++){
+        printf("%d ",heap_.items[i]);
+    }printf("\n");
+}
+
+void increase(struct heap heap_,int key,int value){
+    if(key<heap_.heapSize){
+        heap_.items[key]=value;
+        while(key>0 && heap_.items[key]>heap_.items[getParent(key)]){
+            swap(&(heap_.items[key]),&(heap_.items[getParent(key)]));
+            key = getParent(key);
+        }
+    }
+}
+
+void decrease(struct heap heap_,int key,int value){
+    if(key<heap_.heapSize){
+        heap_.items[key]=value;
+        while(key<heap_.heapSize && heap_.items[key]<heap_.items[getRightChild(key)]){
+            swap(&(heap_.items[key]),&(heap_.items[getRightChild(key)]));
+            key = getRightChild(key);
+        }
+    }
+}
+void printByLength(struct heap heap_){
     printf("Items in the heap are : ");
     for(int i=0;i<heap_.length;i++){
         printf("%d ",heap_.items[i]);
     }printf("\n");
 }
-int main(){
-    int length=7;
-    struct heap heap_ = create(length);
-    for(int i=0;i<length;i++){
-        heap_.items[i]=rand()%length;
+void heapSort(struct heap *heap_){
+    buildHeap(heap_);
+    printByLength(*heap_);
+    for(int i=heap_->heapSize-1;i>=1;i--){
+        swap(&(heap_->items[i]),&(heap_->items[0]));
+        heap_->heapSize=heap_->heapSize-1;
+        maxHeapify(heap_,0);
     }
+    printByLength(*heap_);
+}
+
+int main(){
+    int length=7;   
+    struct heap heap_;
+    heap_ = create(length);
     print(heap_);
     buildHeap(&heap_);
     print(heap_);
+    increase(heap_,6,200);
+    print(heap_);
+    decrease(heap_,0,4);
+    print(heap_);
+    printf("Extracting the max element %d\n",extractMax(&heap_));
+    print(heap_);
+    heap_=create(15);
+    print(heap_);
+    heapSort(&heap_);
     return 0;
 }
