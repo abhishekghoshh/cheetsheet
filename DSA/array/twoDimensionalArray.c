44f0989042c5cc0cbf8617c40b0ab10ead99ad8d
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-09-20 22:30:23 +0530

    array completed

--- DSA/array/twoDimensionalArray.c
+++ DSA/array/twoDimensionalArray.c
@@ -3,32 +3,34 @@
 #include<stdlib.h>
 int main(){
     int row =3,column=4;
+    int i =0,j=0;
     int count =0;
-    // Using an array of pointers
-    // two dimensional array can be thought as a one dimention array of pointers;
-    // where this array has no of elements which is equal to row
-    int *arr[row];
-    
-    // Using pointer to a pointer
-    // We can create an array of pointers also dynamically using a double pointer. 
-    // Once we have an array pointers allocated dynamically, 
-    // we can dynamically allocate memory and for every row like method 2.
-    // Note that arr[i][j] is same as *(*(arr+i)+j)
-    int **arr2 = (int **)malloc(row * sizeof(int *)); 
 
-    //initializing the array now
-    for(int i=0;i<row;i++){
-        arr[i]=(int*)malloc(sizeof(int*));
+    //array of pointer 
+    int *arr[row];
+    for (i=0; i<row; i++){
+         arr[i] = (int *)malloc(column * sizeof(int)); 
     }
-    for(int i = 0;i<row;i++){
-        for(int j=0;j<column;j++){
-            arr[i][j]= (++count)%12;
+
+    //pointer of pointer
+    int **arr = (int **)malloc(row * sizeof(int *)); 
+    for (i=0; i<row; i++){
+         arr[i] = (int *)malloc(column * sizeof(int)); 
+    } 
+
+    //accessing the element 
+    count = 0; 
+    for (i = 0; i <  row; i++){
+        for (j = 0; j < column; j++){
+            arr[i][j] = ++count; // Or *(*(arr+i)+j) = ++count 
         }
     }
-    for(int i = 0;i<row;i++){
-        for(int j=0;j<column;j++){
-            printf("%d ",arr[i][j]);
+      
+  
+    for (i = 0; i <  row; i++){
+        for (j = 0; j < column; j++){
+            printf("%d ", arr[i][j]); 
         }
-        printf("\n");
-    }
+    } 
+         
 }
