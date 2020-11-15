2ab8bec67251846d0bf1cf0cc976955a7b84d94e
noobMasterProgrammer <ghoshabhishek1640@gmail.com>
2020-11-16 01:16:22 +0530

    commit

--- DSA/linkedList/doublyLinkedList.c
+++ DSA/linkedList/doublyLinkedList.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 // #include<conio.h>
 #include<stdlib.h>
-int array[]={1,2,3,4,5};
+// int array[]={1,2,3,4,5};
+#define INVALID -99999
 
 struct doublyLinkedList{
     struct node *start;
@@ -31,10 +32,10 @@ void printListwithAddress(struct doublyLinkedList list){
     printf("The list is :\n");
     if(NULL!=start){
         while(NULL!=start){
-            printf("prev addr = %p , data = %d next addr = %p\n",start->data);
+            printf("prev addr = %p , data = %d, address = %p  ,next addr = %p\n",start->prev,start->data,start,start->next);
             start=start->next;
         }
-    }
+    }printf("\n");
 }
 struct node* getOneNode(int data){
     struct node *temp = (struct node*)malloc(sizeof(struct node*));
@@ -42,26 +43,122 @@ struct node* getOneNode(int data){
     temp->next=NULL;
     temp->prev=NULL;
     return temp;
-}   
+} 
+struct doublyLinkedList initialize(){
+    struct doublyLinkedList list;
+    list.start= NULL;
+    list.start=NULL;
+    list.length=0;
+    return list;
+}  
 struct doublyLinkedList initializeLinkedList(int *array,int count){
+    struct doublyLinkedList list = initialize();
+    struct node *start = NULL,*last=NULL,*prev=NULL;
+    for(int i=0;i<count;i++){
+        last = getOneNode(array[i]);
+        if(NULL==start){
+            start= last;
+            prev=last;
+        }else{
+            prev->next=last;
+            last->prev=prev;
+            prev=last;
+        }
+    }
+    list.start=start;
+    list.length=count;
+    list.last=last;
+    return list;
+}
+void here(){
+    printf("\nI am here\n");
 }
 void insertAtBegin(struct doublyLinkedList *list,int data){
+    if(NULL!=list){
+        struct node *temp=getOneNode(data);
+        list->length=list->length+1;
+        if(NULL!=list->start){
+            temp->next=list->start;
+            list->start->prev=temp;
+        }else{
+            list->last=temp;
+        }
+        list->start=temp;
+    }
 }
 void insertAtEnd(struct doublyLinkedList *list,int data){
+    if(NULL!=list){
+        struct node *temp=getOneNode(data);
+        list->length=list->length+1;
+        if(NULL!=list->start){
+            list->last->next=temp;
+            temp->prev=list->last;
+        }else{
+            list->start=temp;
+        }
+        list->last=temp;
+    }
 }
 int deleteFromBegin(struct doublyLinkedList *list){
+    int data = INVALID;
+    if(NULL!=list){
+        if(NULL!=list->start){
+            struct node *temp =list->start;
+            data=temp->data;
+            list->start=temp->next;
+            if(NULL==temp->next){
+                list->last=NULL;
+            }
+            free(temp);
+            list->length=list->length-1;
+            list->start->prev=NULL;
+        }
+    }
+    return data;
 }
 int deleteFromEnd(struct doublyLinkedList *list){
+    int data = INVALID;
+    if(NULL!=list){
+        if(NULL!=list->start){
+            struct node *temp=list->start;
+            while(NULL!=temp->next){
+                temp=temp->next;
+            }
+            if(NULL==temp->next && NULL==temp->prev){
+                list->start=NULL;
+                list->last=NULL;
+            }else{
+                list->last=temp->prev;
+                list->last->next=NULL;
+            }
+            data=temp->data;
+            free(temp);
+            list->length=list->length-1;
+        }
+    }
+    return data;
 }
-void insertAtN(struct doublyLinkedList *list,int position,int data){
-}
-int deleteAtN(struct doublyLinkedList *list,int position){
-}
-void reverse(struct doublyLinkedList *list){
-}
-struct doublyLinkedList initialize(){
-}
+// void insertAtN(struct doublyLinkedList *list,int position,int data){
+// }
+// int deleteAtN(struct doublyLinkedList *list,int position){
+// }
+// void reverse(struct doublyLinkedList *list){
+// }
+
 int main(){
-    struct doublyLinkedList list;
+    struct doublyLinkedList list = initialize();
+    int array[] = {1,2,3,4,5};
+    // int count = ((sizeof(array))/sizeof(int));
+    // list=initializeLinkedList(array,count);
+    insertAtBegin(&list,6);
+    insertAtEnd(&list,7);
+    printList(list);
+    printListwithAddress(list);
+    printf("delete from begining : %d\n",deleteFromBegin(&list));
+    // printList(list);
+    // printListwithAddress(list);
+    printf("delete from end : %d\n",deleteFromEnd(&list));
+    printList(list);
+    printListwithAddress(list);
     return 0;
 }
